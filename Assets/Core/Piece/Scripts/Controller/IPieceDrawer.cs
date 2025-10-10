057c784c959c1be38314cd99e18fb5cc4bb8c91d
Kakassss <celikonur988@gmail.com>
2025-10-10 16:10:15 +0300

    PieceDrawer: namescapes added and interface seperation

---
+++ Assets/Core/Piece/Scripts/Controller/IPieceDrawer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Core.Piece.Scripts.View;
+using UnityEngine;
+
+namespace Core.Piece.Scripts.Controller
+{
+    public interface IPieceDrawer
+    {
+        public List<PieceView> DrawPieces(List<Data.Piece> pieces, List<Vector3> snapPoints,PieceView pieceViewPrefab, Transform parent);
+    }
+}
--- Assets/Core/Piece/Scripts/Controller/PieceDrawer.cs
+++ Assets/Core/Piece/Scripts/Controller/PieceDrawer.cs
@@ -1,54 +1,51 @@
 using System.Collections.Generic;
 using Core.Level.Controller;
 using Core.Piece.Scripts.Controller.Interfaces;
-using Core.Piece.Scripts.Data;
 using Core.Piece.Scripts.View;
 using UnityEngine;
 using Zenject;
 
-public interface IPieceDrawer
+namespace Core.Piece.Scripts.Controller
 {
-    public List<PieceView> DrawPieces(List<Piece> pieces, List<Vector3> snapPoints,PieceView pieceViewPrefab, Transform parent);
-}
-
-public class PieceDrawer : IPieceDrawer
-{
-    private IInstantiator _instantiator;
-    private IPieceSpawnPositionController _pieceSpawnPositionController;
-    private ILevelCompletionController _levelCompletionController;
-
-    public PieceDrawer(IPieceSpawnPositionController pieceSpawnPositionController,
-        ILevelCompletionController levelCompletionController, IInstantiator instantiator)
+    public class PieceDrawer : IPieceDrawer
     {
-        _instantiator = instantiator;
-        _levelCompletionController = levelCompletionController;
-        _pieceSpawnPositionController = pieceSpawnPositionController;
-    }
+        private IInstantiator _instantiator;
+        private IPieceSpawnPositionController _pieceSpawnPositionController;
+        private ILevelCompletionController _levelCompletionController;
 
-    public List<PieceView> DrawPieces(List<Piece> pieces, List<Vector3> snapPoints,PieceView pieceViewPrefab, Transform parent)
-    {
-        List<PieceView> spawnedPieces = new List<PieceView>();
-
-        for (int i = 0; i < pieces.Count; i++)
+        public PieceDrawer(IPieceSpawnPositionController pieceSpawnPositionController,
+            ILevelCompletionController levelCompletionController, IInstantiator instantiator)
         {
-            Piece piece = pieces[i];
-            Mesh mesh = piece.CreateMesh();
-
-            var pieceGo = _instantiator.InstantiatePrefabForComponent<PieceView>(pieceViewPrefab, parent);
-            pieceGo.name = "Piece_" + piece.ID;
-            mesh.name = "Piece_" + piece.ID + "_Mesh";
-
-            pieceGo.SetPiece(piece.Triangles, snapPoints);
-            pieceGo.SetMesh(mesh);
-
-            pieceGo.transform.position = _pieceSpawnPositionController.GetSpawnPosition();
-            _pieceSpawnPositionController.PieceMovementTween(pieceGo.transform);
-            
-            spawnedPieces.Add(pieceGo);
+            _instantiator = instantiator;
+            _levelCompletionController = levelCompletionController;
+            _pieceSpawnPositionController = pieceSpawnPositionController;
         }
 
-        _levelCompletionController.SetLevelTarget(spawnedPieces.Count);
+        public List<PieceView> DrawPieces(List<Data.Piece> pieces, List<Vector3> snapPoints,PieceView pieceViewPrefab, Transform parent)
+        {
+            List<PieceView> spawnedPieces = new List<PieceView>();
+
+            for (int i = 0; i < pieces.Count; i++)
+            {
+                Data.Piece piece = pieces[i];
+                Mesh mesh = piece.CreateMesh();
+
+                var pieceGo = _instantiator.InstantiatePrefabForComponent<PieceView>(pieceViewPrefab, parent);
+                pieceGo.name = "Piece_" + piece.ID;
+                mesh.name = "Piece_" + piece.ID + "_Mesh";
+
+                pieceGo.SetPiece(piece.Triangles, snapPoints);
+                pieceGo.SetMesh(mesh);
+
+                pieceGo.transform.position = _pieceSpawnPositionController.GetSpawnPosition();
+                _pieceSpawnPositionController.PieceMovementTween(pieceGo.transform);
+            
+                spawnedPieces.Add(pieceGo);
+            }
+
+            _levelCompletionController.SetLevelTarget(spawnedPieces.Count);
         
-        return spawnedPieces;
+            return spawnedPieces;
+        }
     }
 }
